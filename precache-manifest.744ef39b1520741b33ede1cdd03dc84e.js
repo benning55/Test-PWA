8afe815329a0ca3a6e6694fdd00f82a717a7719a
Benning <bmaisonti@gmail.com>
2021-02-15 23:29:38 +0700

    deploy

---
+++ precache-manifest.744ef39b1520741b33ede1cdd03dc84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b31faeb26d9383ce595",
+    "url": "/promptpay-pwa/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/promptpay-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "14ebced8bc8b45c28d951beedd621516",
+    "url": "/promptpay-pwa/index.html"
+  },
+  {
+    "revision": "4b31faeb26d9383ce595",
+    "url": "/promptpay-pwa/js/app.724b117f.js"
+  },
+  {
+    "revision": "7f0f8e5a56f2c037fb6d",
+    "url": "/promptpay-pwa/js/chunk-vendors.93cfb4a2.js"
+  },
+  {
+    "revision": "9f23fe95d325365e86183e0480329c1d",
+    "url": "/promptpay-pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/promptpay-pwa/robots.txt"
+  }
+]);
